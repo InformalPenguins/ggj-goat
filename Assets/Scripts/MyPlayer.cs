cfb7f0881e15a01a78cc63c631d73411fbc298a5
tonymtz <atony.mtz@gmail.com>
2016-01-30 04:37:46 -0700

    New Scene
    Platform game :smile:

---
+++ Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyPlayer : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+
+    private Animator myAnimator;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private float groundRadius;
+
+    private bool facingLeft;
+
+    private bool isGrounded;
+
+    private bool isJump;
+
+    [SerializeField]
+    private bool airControl;
+
+    [SerializeField]
+    private float jumpForce;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        facingLeft = true;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+	void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        isGrounded = IsGrounded();
+
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
+
+        HandleLayers();
+
+        ResetValues();
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        Debug.Log(myRigidbody.velocity.y);
+
+        if (myRigidbody.velocity.y < 0)
+        {
+            myAnimator.SetBool("land", true);
+        }
+
+        if (isGrounded || airControl)
+        {
+            myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+        }
+
+        if (isGrounded && isJump)
+        {
+            isGrounded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+            myAnimator.SetTrigger("jump");
+        }
+
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJump = true;
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && facingLeft || horizontal < 0 && !facingLeft)
+        {
+            facingLeft = !facingLeft;
+            Vector3 myScale = transform.localScale;
+            myScale.x *= -1;
+            transform.localScale = myScale;
+        }
+    }
+
+    private void ResetValues()
+    {
+        isJump = false;
+    }
+
+    private bool IsGrounded()
+    {
+        if (myRigidbody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        myAnimator.ResetTrigger("jump");
+                        myAnimator.SetBool("land", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void HandleLayers()
+    {
+        if (isGrounded)
+        {
+            myAnimator.SetLayerWeight(1, 0);
+        }
+        else
+        {
+            myAnimator.SetLayerWeight(1, 1);
+        }
+    }
+}
